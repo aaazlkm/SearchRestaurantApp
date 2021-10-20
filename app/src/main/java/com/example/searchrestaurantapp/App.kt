9b0f249548edc5d4d8d92702ac3c5dfdaba0a632
aaazlkm <40765910+aaazlkm@users.noreply.github.com>
2021-10-20 17:55:13 +0900

    App Startup 導入する #4

--- app/src/main/java/com/example/searchrestaurantapp/App.kt
+++ app/src/main/java/com/example/searchrestaurantapp/App.kt
@@ -2,18 +2,6 @@ package com.example.searchrestaurantapp
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-import timber.log.Timber
 
 @HiltAndroidApp
-class App : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        setupTimber()
-    }
-
-    private fun setupTimber() {
-        if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
-        }
-    }
-}
+class App : Application()
---
+++ app/src/main/java/com/example/searchrestaurantapp/LoggerInitializer.kt
@@ -0,0 +1,17 @@
+package com.example.searchrestaurantapp
+
+import android.content.Context
+import androidx.startup.Initializer
+import timber.log.Timber
+
+class LoggerInitializer : Initializer<Unit> {
+    override fun create(context: Context) {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
+    override fun dependencies(): List<Class<out Initializer<*>>> {
+        return emptyList()
+    }
+}
